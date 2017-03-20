fb3dfbec7f69428768468ea3c82e2dc03c4fbc8c
hindAhmad <9boo7h1995@gmail.com>
2017-03-20 12:36:40 +0300

    Map Activity

--- app/src/main/java/ksu/airuino/MapActivity.java
+++
@@ -1,8 +0,0 @@
-package ksu.airuino;
-
-/**
- * Created by hind on 3/20/2017 AD.
- */
-
-public class MapActivity {
-}
---
+++ app/src/main/res/MapsActivity.java
@@ -0,0 +1,6 @@
+/**
+ * Created by hind on 3/20/2017 AD.
+ */
+
+public class MapsActivity {
+}
